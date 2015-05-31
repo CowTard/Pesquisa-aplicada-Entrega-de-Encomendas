ace0485f5a4b0570173bf8f3cca961ebf5e234db
Gustavo Silva <gustavo.silva@fe.up.pt>
2015-05-31 16:04:30 +0100

    Classe Estado e início do A*

--- src/algoritmos/Astar.java
+++ src/algoritmos/Astar.java
@@ -1,5 +1,15 @@
 package algoritmos;
 
-public class Astar {
+import java.util.ArrayList;
 
+import elementos.Estado;
+
+public class Astar {
+	public void executar(Estado inicial) {
+		ArrayList<Estado> listaAberta = new ArrayList<Estado>();
+		ArrayList<Estado> listaFechada = new ArrayList<Estado>();
+		
+		inicial.f = 0;
+		listaAberta.add(inicial);
+	}
 }
---
+++ src/elementos/Estado.java
@@ -0,0 +1,9 @@
+package elementos;
+
+public class Estado {
+	public Nó nóAtual;
+	public Veículo veículo;
+	public int encomendasPorEntregar;
+	
+	public int f, g, h;
+}
